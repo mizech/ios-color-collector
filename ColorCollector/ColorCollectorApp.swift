ab331181a139c455a9048f8a9d7b51c2cbe0fb03
mizech <michael.zech@hotmail.de>
2026-06-27 10:26:00 +0200

    Initial Commit

---
+++ ColorCollector/ColorCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCollectorApp.swift
+//  ColorCollector
+//
+//  Created by Michael on 27.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ColorCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
